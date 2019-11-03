3a6847851433d5b43430ddfb07a27677096d8f53
André Gonçalves <andgcv@gmail.com>
2019-11-03 17:57:42 +0000

    Added geocode and forecast utils for the weather endpoint

---
+++ src/utils/geocode.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    // Use encodeURIComponent() incase someone searches for an address that contains special characters, like '?', which becomes '%3F' with this function
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?limit=1&access_token=pk.eyJ1IjoiYW5kZ2N2IiwiYSI6ImNrMjdqam84ZDBhOWMzbG9icDMxdTBkM2cifQ._fy5QQ3lrAq1TIu5i4vQ6A`
+
+    request({url, json: true}, (error, {body} = {}) => {
+        if (error) {
+            callback('Unable to connect to location service!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location, try again with a different searching term.', undefined)
+        } else {
+            callback(undefined, {
+                longitude: body.features[0].center[0],
+                latitude: body.features[0].center[1],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
