7db9c2ba0f42710eb6e5eb3f5769b77677005b64
André Gonçalves <andgcv@gmail.com>
2019-11-03 19:11:38 +0000

    User can input location through a form to get location, improved UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,28 @@
-// fetch the url, then run the function
-fetch('http://localhost:3000/weather?address=lisbon')
+const weatherForm = document.querySelector('form')
+const textInput = document.querySelector('input[type=text]')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+weatherForm.addEventListener('submit', (event) => {
+    // Grab event to prevent default behaviour (refresh page)
+    event.preventDefault()
+
+    const location = textInput.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    // Fetch the url, then run the function
+    fetch(`http://localhost:3000/weather?address=${location}`)
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+        if (data.error) {
+            messageOne.textContent = data.error
+        } else {
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        }
+        console.log(data)
+    })
+})
